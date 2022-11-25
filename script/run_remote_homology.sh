321a60953f270ccd5221be655b292168a99290fd
cheng-siyuan <50662014+cheng-siyuan@users.noreply.github.com>
2022-11-25 10:34:38 +0800

    Update run_remote_homology.sh

--- script/run_remote_homology.sh
+++ script/run_remote_homology.sh
@@ -1,15 +1,16 @@
 cd ../
 
+# Training on 1 v100
 nohup sh run_main.sh \
       --model ./model/remote_homology/ProtBertModel \
       --output_file remote_homology-ProtBert \
       --task_name remote_homology \
       --do_train False \
-      --epoch 10 \
+      --epoch 20 \
       --mean_output True \
       --optimizer AdamW \
-      --per_device_batch_size 1 \
-      --gradient_accumulation_steps 64 \
+      --per_device_batch_size 8 \
+      --gradient_accumulation_steps 8 \
       --eval_step 50 \
       --eval_batchsize 8 \
       --warmup_ratio 0.08 \
