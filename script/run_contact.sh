83b2a17dd31c5e4c29c362ad23efe339b2ef61ea
cheng-siyuan <50662014+cheng-siyuan@users.noreply.github.com>
2022-11-25 10:29:38 +0800

    Update run_contact.sh

--- script/run_contact.sh
+++ script/run_contact.sh
@@ -1,14 +1,15 @@
 cd ../
 
+# Training on 4 v100 GPUs
 nohup sh run_main.sh \
       --model ./model/contact/OntoproteinModel \
       --output_file contact-ontoprotein \
       --task_name contact \
       --do_train False \
-      --epoch 1 \
+      --epoch 10 \
       --optimizer AdamW \
       --per_device_batch_size 1 \
-      --gradient_accumulation_steps 1 \
+      --gradient_accumulation_steps 2 \
       --eval_step 50 \
       --eval_batchsize 1 \
       --warmup_ratio 0.08 \
